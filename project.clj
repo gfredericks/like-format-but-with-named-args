2c39a1de0df4defc4d7639581de4cb994a77e95d
Gary Fredericks <fredericksgary@gmail.com>
2015-03-05 13:29:59 -0600

    Clojars is deploy repo

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/gfredericks/like-format-but-with-named-args"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]])
