d453399b220f6f3a094438a8d19d7ea0e0f21b7f
Gary Fredericks <fredericksgary@gmail.com>
2015-03-05 20:06:35 -0600

    Rewrite everything

--- src/com/gfredericks/like_format_but_with_named_args.clj
+++ src/com/gfredericks/like_format_but_with_named_args.clj
@@ -1,44 +1,52 @@
 (ns com.gfredericks.like-format-but-with-named-args)
 
 (defn parse-named-format-string
-  "Input is a format string similar to that accepted by clojure.core/format, but
-  with names on each formatting expression. Returns a function that accepts a map
-  and returns a formatted string. E.g.,:
-
-  ((parse-named-format-string \"Hello my name is %name~s.\")
-   {:name \"Joe Biden\"})
-  => \"Hello my name is Joe Biden.\"
-
-  Names are terminated with a \\~ character, and immediately following the
-  \\~ should be a format specifier as per clojure.core/format.
-
-  Names are always interpreted as keywords."
+  "Given a format string as described by the docstring on the named-format
+  function, returns a pair [clojure-format-string names] where
+  `clojure-format-string` is a similar format string suitable to pass
+  to clojure.core/format, and `names` is a vector of keywords
+  representing the names used in the format specifiers in the input
+  string."
   [format-str]
-  (loop [out-str ""
-         keys []
-         ^String
-         s format-str]
-    (if (empty? s)
-      (if (empty? keys)
-        (constantly (format out-str))
-        (let [f (apply juxt keys)]
-          (fn [m]
-            (apply format out-str (f m)))))
-      (let [i (.indexOf s "%")]
-        (if (neg? i)
-          (recur (str out-str s) keys nil)
-          (let [s-before (subs s 0 i)
-                s-after (subs s i)]
-            (if (= [\% \%] (take 2 s-after))
-              (recur (str out-str s-before "%%") keys (subs s-after 2))
-              (if-let [[_ keyname remaining] (re-matches #"(?s)%([-_a-zA-Z]+)~(.*)" s-after)]
-                (recur (str out-str s-before "%")
-                       (conj keys (keyword keyname))
-                       remaining)
-                (throw (ex-info "Bad format string!" {:arg format-str}))))))))))
+  (loop [out-str "", keys [], ^String unparsed format-str]
+    (let [i (.indexOf unparsed "%")]
+      (if (neg? i)
+        [(str out-str unparsed) keys]
+        (let [before-% (subs unparsed 0 i)
+              at-and-after-% (subs unparsed i)]
+          (if (= [\% \%] (take 2 at-and-after-%))
+            (recur (str out-str before-% "%%") keys (subs at-and-after-% 2))
+            (if-let [[_ keyname remaining] (re-matches #"(?s)%([^\s~%]+)~(.*)" at-and-after-%)]
+              (recur (str out-str before-% "%")
+                     (conj keys (keyword keyname))
+                     remaining)
+              (throw (ex-info "Bad format string!" {:arg format-str})))))))))
 
 (defn named-format
-  "Like clojure.core/format but uses named args. See the docstring on
-  parse-named-format-string for details."
-  [s m]
-  ((parse-named-format-string s) m))
+  "Like clojure.core/format but uses named args. `format-string` is
+  similar to that accepted by clojure.core/format, but with names on
+  each formatting expression. `arg-map` is a map with keyword keys
+  matching the names used in the format string. E.g.,:
+
+  (named-format \"Hello my name is %name~s and I am %height~.3f feet tall.\"
+                {:name   \"Joe Biden\"
+                 :height (* 2 Math/PI)})
+  ;; => \"Hello my name is Joe Biden and I am 6.283 feet tall.\"
+
+  Names are terminated with a \\~ character, and immediately following the
+  \\~ should be a format specifier as per clojure.core/format."
+  ;; inlined version so we can parse at compile time if possible. Not
+  ;; sure how to do this without essentially writing the function
+  ;; three times. Whatever.
+  {:inline (fn [format-string arg-map]
+             (if (string? format-string)
+               (let [[clojure-format-string names] (parse-named-format-string format-string)]
+                 `(let [arg-map# ~arg-map]
+                    (apply format ~clojure-format-string (map #(get arg-map# %) ~names))))
+               `(let [[clojure-format-string# names#] (parse-named-format-string ~format-string)
+                      arg-map# ~arg-map]
+                  (apply format clojure-format-string# (map #(get arg-map# %) names#)))))
+   :inline-arities #{2}}
+  [format-string arg-map]
+  (let [[clojure-format-string names] (parse-named-format-string format-string)]
+    (apply format clojure-format-string (map #(get arg-map %) names))))
