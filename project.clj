7ae5df788d5efe5b272adeeed9fc664b411d1341
Gary Fredericks <fredericksgary@gmail.com>
2018-08-04 20:09:28 -0500

    Upgrade clojure dep

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" :clojars]]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]])
